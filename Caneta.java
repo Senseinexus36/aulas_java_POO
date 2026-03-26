c46074413b7ef2fc11171758f77f68fe27f79c87
Josemilson Sousa <57382472+JosemilsonSousa@users.noreply.github.com>
2026-03-25 22:25:59 -0300

    enviando arquivos

---
+++ Caneta.java
@@ -0,0 +1,33 @@
+package Aula02;
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void status(){
+        System.out.print(" modelo " + this.modelo);
+        System.out.print(" uma caneta " + this.cor);
+        System.out.print(" ponta " + this.ponta);
+        System.out.print(" carga " + this.carga);
+        System.out.print(" esta tampada?" + this.tampada);
+    }
+    void rabiscar(){
+        if (this.tampada == true) {
+            System.out.println("n posso rabiscar");
+        }   
+        else {
+            System.out.println("rabiscando");
+        }
+
+    }
+    void tampar(){
+        this.tampada = true;
+
+    }
+    void destampar(){
+        this.tampada = false;
+
+    }
+}
