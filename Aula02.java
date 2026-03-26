931e475574d3251cf860e81b5158fe37f66422e0
Josemilson Sousa <57382472+JosemilsonSousa@users.noreply.github.com>
2026-03-25 22:29:51 -0300

    correção do arquivo aula02

--- Aula02.java
+++ Aula02.java
@@ -5,7 +5,7 @@ public class Aula02 {
     public static void main(String[] args) {
         Caneta c1 = new Caneta();
 
-        c1.modelo = "Caneta Bic";
+        c1.modelo = "Caneta fabricastelo";
         c1.cor = "azul";
         c1.ponta =  0.5f;
         c1.tampada = false;
